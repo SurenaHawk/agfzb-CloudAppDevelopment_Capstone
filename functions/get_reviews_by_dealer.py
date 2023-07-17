5e38d4a05ef79185b01aec1a18b8142c7d61926d
SurenaHawk <khaboshan.farnoud@gmail.com>
2023-07-17 05:23:03 -0400

    Creates all the file

--- functions/get_reviews_by_dealer.py
+++ functions/get_reviews_by_dealer.py
@@ -2,8 +2,7 @@ import sys
 from ibmcloudant.cloudant_v1 import CloudantV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from ibm_cloud_sdk_core import ApiException
-import requests
-import json
+
 
 def main(dict):
     COUCH_URL = "https://7ba488ca-c0e9-4f83-9c01-09dffdd47f94-bluemix.cloudantnosqldb.appdomain.cloud"
@@ -13,15 +12,19 @@ def main(dict):
         authenticator = IAMAuthenticator(apikey=IAM_API_KEY)
         client = CloudantV1(authenticator=authenticator)
         client.set_service_url(COUCH_URL)
-        review_database = client.post_all_docs(db="reviews").get_result()
-        reviews_list = reviews["rows"]
-        dealership_id = params.get("dealership_id")
+        dealership_id = dict.get("dealership")
+        reviews = client.post_all_docs(db="reviews", include_docs=True).get_result()
         res = []
-        for review in reviews_list:
-            if review["doc"]["dealership_id"] == dealership_id:
-                res.append(reivew)
-        return {
+        for row in reviews["rows"]:
+            doc = row["doc"]
+            if "dealership" in doc and doc["dealership"] == dealership_id:
+                res.append(doc)
+        return{
             "reviews": res
         }
-    except Exception as ex:
-        return {"error": str(ex)}
+    except Exception as e:
+        return{
+            "code": 500,
+            "message": "Something went wrong on the server",
+            "error": str(e)
+        }
--- functions/post_review.py
+++ functions/post_review.py
@@ -7,21 +7,34 @@ def main(dict):
     COUCH_URL = "https://7ba488ca-c0e9-4f83-9c01-09dffdd47f94-bluemix.cloudantnosqldb.appdomain.cloud"
     IAM_API_KEY = "syHHOFlKoEbur35iLvyzCR4WmCFtnkZZMsW1kdbPebG9"
     COUCH_USERNAME = "7ba488ca-c0e9-4f83-9c01-09dffdd47f94-bluemix"
+    review = {
+            "id": 1114,
+            "name": "Upkar Lidder",
+            "dealership": 15,
+            "review": "Great service!",
+            "purchase": False,
+            "another": "field",
+            "purchase_date": "02/16/2021",
+            "car_make": "Audi",
+            "car_model": "Car",
+            "car_year": 2021
+    }
     try:
         authenticator = IAMAuthenticator(apikey=IAM_API_KEY)
         client = CloudantV1(authenticator=authenticator)
         client.set_service_url(COUCH_URL)
-        reviews = client.post_all_docs(db="reviews", include_docs=True).get_result()
-        res = [row['doc'] for row in reviews['rows']]
-        return{
-            "reviews": res
+        new_review = client.post_document(db="reviews", document=review)
+        res = new_review.get_result()
+        result = {
+            "headers": {"Content-Type": "application/json"},
+            "body": {"message": "Review posted"}
+        }
+        return {
+            "review": result
         }
     except Exception as e:
-        return{
+        return {
             "code": 500,
             "message": "Something went wrong on the server",
             "error": str(e)
-        }
-
-
-
+        }        
